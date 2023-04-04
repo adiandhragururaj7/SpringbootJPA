72297fd46924380d60720ba11836d770d088b29d
adiandhragururaj7 <adiandhragururaj7@gmail.com>
2023-04-04 14:14:46 +0530

    First commit- Made a connecton with database using JPA

---
+++ src/main/java/com/example/hibernatejpa/HibernateJpaApplication.java
@@ -0,0 +1,21 @@
+package com.example.hibernatejpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HibernateJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateJpaApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args){
+        return runner ->{
+            System.out.println("success");
+        };
+    }
+
+}
